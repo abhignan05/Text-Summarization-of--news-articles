909fb87ffab91b4fd08ef7dc672bd573b0a44c6a
Abhignan Bollam <abhignanbollam@gmail.com>
2025-03-18 12:45:53 +0530

    hhllo

--- summarizer.py
+++ summarizer.py
@@ -16,8 +16,8 @@ def generate_summary(text):
         min_len = 80   # Ensure at least 4-5 lines
         max_len = 120
     else:  # If the article is short (1-2 paragraphs)
-        min_len = 40   # Ensure at least 2-3 lines
-        max_len = 60
+        min_len = 20   # Ensure at least 2-3 lines
+        max_len = 50
 
     # Generate summary
     summary = summarizer(text, max_length=max_len, min_length=min_len, length_penalty=1.0, do_sample=False)
