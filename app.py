8673b9a610d93954d5ed8fa468694caefc06a784
Abhignan Bollam <abhignanbollam@gmail.com>
2025-03-26 10:22:09 +0530

    hlo

--- app.py
+++ app.py
@@ -12,18 +12,15 @@ def home():
 def predict():
     if request.method == "POST":
         text = request.form.get("news_article", "").strip()
-        
 
         if not text:
             return render_template('predict.html', summary="⚠️ Please enter valid text.", input_text="")
 
         summary = summarize_text(text)  
-        
-        
         return render_template('predict.html', summary=summary, input_text=text)
 
     return render_template('predict.html', summary="", input_text="")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  
+    port = int(os.environ.get("PORT", 8080))  
     app.run(host="0.0.0.0", port=port)
